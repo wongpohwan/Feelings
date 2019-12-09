ffaea3901ad24a64f86656be3a9c334bc357efa4
wongpohwan <wongpw-wm17@student.tarc.edu.my>
2019-12-09 12:08:04 +0800

    Initial commit

---
+++ app/src/main/java/com/example/feelings/FeelingDAO.kt
@@ -0,0 +1,15 @@
+package com.example.feelings
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface FeelingDAO {
+    @Insert
+    suspend fun insertFeeling(feeling: Feeling)
+
+    @Query("SELECT * FROM feeling")
+    fun getFeelings() : LiveData<Feeling>
+}
---
+++ app/src/main/java/com/example/feelings/FeelingDatabase.kt
@@ -0,0 +1,35 @@
+package com.example.feelings
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = arrayOf(Feeling::class), version = 1)
+public abstract class FeelingDatabase : RoomDatabase() {
+    //Create an instance of DAO
+    abstract fun feelingDao(): FeelingDAO
+
+    companion object {
+        //Ensure only one instance of the database is created
+        @Volatile
+        private var INSTANCE : FeelingDatabase? = null
+
+        //Function to obtain or create the database
+        fun getDatabase(context: Context): FeelingDatabase {
+            var tempDB = INSTANCE
+            if(tempDB != null) {
+                return tempDB
+            }
+            synchronized(this){
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    FeelingDatabase::class.java,
+                    "feeling_db"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+}
